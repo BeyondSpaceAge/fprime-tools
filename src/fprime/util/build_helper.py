77665d291520d50dffdaec7473a079a457392a81
Sourcery AI <>
2022-11-16 20:41:54 +0000

    'Refactored by Sourcery'

--- src/fprime/util/build_helper.py
+++ src/fprime/util/build_helper.py
@@ -116,9 +116,8 @@ def validate(parsed, unknown):
             match.group(1): match.group(2)
             for match in [CMAKE_REG.match(arg) for arg in unknown]
         }
-        cmake_args.update(d_args)
+        cmake_args |= d_args
         unknown = [arg for arg in unknown if not CMAKE_REG.match(arg)]
-    # Build type only for generate, jobs only for non-generate
     elif parsed.command in Target.get_all_targets():
         parsed.settings = None  # Force to load from cache if possible
         make_args["--jobs"] = 1 if parsed.jobs <= 0 else parsed.jobs
@@ -197,9 +196,9 @@ def parse_args(args):
         subparsers, common_parser, HelpText
     )
     fpp_runners, fpp_parsers = add_fpp_parsers(subparsers, common_parser)
-    parsers.update(fbuild_parsers)
+    parsers |= fbuild_parsers
     parsers.update(fpp_parsers)
-    runners.update(fbuild_runners)
+    runners |= fbuild_runners
     runners.update(fpp_runners)
     runners.update(add_special_parsers(subparsers, common_parser, HelpText))
 
